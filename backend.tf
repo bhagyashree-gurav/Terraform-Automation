c42420797f3ba4c9f00d8c8e8bded3abef73da73
bhagyashree-gurav <bhagyashrigurav95@gmail.com>
2025-06-30 23:55:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-aws-devops-2"
+    bucket = "mydev-project-terraform-sample-batch-aws-devops-28"
     key = "main"
     region = "us-east-1"
    
