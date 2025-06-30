f5dd1be94fa3155efcac0629f3cff58ef83ad8e7
bhagyashree-gurav <bhagyashrigurav95@gmail.com>
2025-06-30 22:32:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-backend-project-1"
-    key = "state/terraform.tfstate"
+    key = "terraform/state.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
     use_lockfile = true
